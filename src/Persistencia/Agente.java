37f9443f0d25233d3fc62bc9f02ae2c63178f6ec
AaronPecesG <aaron.peces@alu.uclm.es>
2020-12-08 17:29:24 +0100

    Creacion del agente de la base de datos

--- src/Persistencia/Agente.java
+++ src/Persistencia/Agente.java
@@ -1,8 +1,42 @@
 package Persistencia;
+
+import java.sql.*;
+
 //AARON
 public class Agente {
-
-	String user;
-	String password;
+	//Creamos una variable de conexion estatica ya que vamos a utilizar el patron singleton
+	private static Connection conexion = null;
+    private String url;
+    private String driver;
+    private String usuario;
+    private String password;
+    
+    //Constructor
+    private Agente() {
+    	String url = "jdbc:mysql://servidorpecespi.hopto.org:3306/restaurante";
+    	 String driver = "com.mysql.jdbc.Driver";
+    	 String usuario = "isoft2";
+    	 String password = "isoft2c01";
+		try {
+			Class.forName(driver);
+			conexion = DriverManager.getConnection(url,usuario,password);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    //Metodos para abrir y cerrar la conexión:
+    public static Connection getConnection(){
+    	if (conexion == null) {
+    		new Agente();
+    	}
+    	return conexion;
+    }
+    
+    public static void closeConnection() throws SQLException {
+    	if(conexion != null) {
+    		conexion.close();
+    	}
+    }
 
 }
