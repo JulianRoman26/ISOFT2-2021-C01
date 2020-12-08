983c5763488669c2bec9f306b9752d3895fae726
AaronPecesG <aaron.peces@alu.uclm.es>
2020-12-08 22:07:43 +0100

    Agente 1.2

--- src/Persistencia/Agente.java
+++ src/Persistencia/Agente.java
@@ -9,17 +9,17 @@ public class Agente {
 	private static Connection conexion = null;
 	private static PreparedStatement ps;
 	private static ResultSet rs;
-  private String url;
-  private String driver;
-  private String usuario;
-  private String password;
-  
-  //Constructor
-  private Agente() {
-  	 String url = "jdbc:mysql://servidorpecespi.hopto.org:3306/cla";
-  	 String driver = "com.mysql.cj.jdbc.Driver";
-  	 String usuario = "isoft2";
-  	 String password = "isoft2c01";
+	private String url;
+	private String driver;
+	private String usuario;
+	private String password;
+
+	//Constructor
+	private Agente() {
+		String url = "jdbc:mysql://servidorpecespi.hopto.org:3306/restaurante";
+		String driver = "com.mysql.cj.jdbc.Driver";
+		String usuario = "isoft2";
+		String password = "isoft2c01";
 		try {
 			Class.forName(driver);
 			conexion = DriverManager.getConnection(url,usuario,password);
@@ -27,46 +27,46 @@ public class Agente {
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
-  }
-  
-  //Metodos para la interaccion con la base de datos.
-  public static Connection getConnection(){
-  	if (conexion == null) {
-  		new Agente();
-  	}
-  	return conexion;
-  }
-  
-  public static void closeConnection() throws SQLException {
-  	if(conexion != null) {
-  		conexion.close();
-  	}
-  }
-  
-  //Método para insertar registros en la base de dato.
-  public static void insertar(String comando) {
-  	if(conexion == null) {
-  		try {
-  			getConnection();
-  			ps = conexion.prepareStatement(comando);
-  			int res = ps.executeUpdate();
-  			if(res>0) {
-  				System.out.println("Persona guardada");
-  			}else System.out.println("Error al guardar persona");
-  			conexion.close();
-  		}catch(Exception e) {
-  			System.err.print(e);
-  		}
-  	}
-  }
-  
-  //Método para realizar consultas en la base de datos.
-  public static void buscar(String comando) {
-  	if(conexion == null) {
-  		try {
-  			getConnection();
-  			ps = conexion.prepareStatement(comando);
-  			rs = ps.executeQuery();
+	}
+
+	//Metodos para la interaccion con la base de datos.
+	public static Connection getConnection(){
+		if (conexion == null) {
+			new Agente();
+		}
+		return conexion;
+	}
+
+	public static void closeConnection() throws SQLException {
+		if(conexion != null) {
+			conexion.close();
+		}
+	}
+
+	//Método para insertar registros en la base de dato.
+	public static void insertar(String comando) {
+		if(conexion == null) {
+			try {
+				getConnection();
+				ps = conexion.prepareStatement(comando);
+				int res = ps.executeUpdate();
+				if(res>0) {
+					System.out.println("Persona guardada");
+				}else System.out.println("Error al guardar persona");
+				conexion.close();
+			}catch(Exception e) {
+				System.err.print(e);
+			}
+		}
+	}
+
+	//Método para realizar consultas en la base de datos.
+	public static void buscar(String comando) {
+		if(conexion == null) {
+			try {
+				getConnection();
+				ps = conexion.prepareStatement(comando);
+				rs = ps.executeQuery();
 				while(rs.next()) {
 					ResultSetMetaData rsmd = rs.getMetaData();
 					for(int i=1; i<= rsmd.getColumnCount();i++) {
@@ -82,42 +82,42 @@ public class Agente {
 					}
 					System.out.println();
 				}
-  		}catch(Exception e) {
-  			System.err.print(e);
-  		}
-  	}
-  }
-  
-  //Método para modificar las tablas.
-  public static void modificar(String comando) {
-  	if(conexion == null) {
-  		try {
-  			getConnection();
-  			ps = conexion.prepareStatement(comando);
-  			int res = ps.executeUpdate();
-  			if(res>0) {
-  				System.out.println("Persona Modificada");
-  			}else System.out.println("Error al modificar persona");
-  			conexion.close();
-  		}catch(Exception e) {
-  			System.err.print(e);
-  		}
-  	}
-  }
-  
-  public static void eliminar(String comando) {
-  	if(conexion == null) {
-  		try {
-  			getConnection();
-  			ps = conexion.prepareStatement(comando);
-  			int res = ps.executeUpdate();
-  			if(res>0) {
-  				System.out.println("Persona eliminada");
-  			}else System.out.println("Error al eliminar persona");
-  			conexion.close();
-  		}catch(Exception e) {
-  			System.err.print(e);
-  		}
-  	}
-  }
+			}catch(Exception e) {
+				System.err.print(e);
+			}
+		}
+	}
+
+	//Método para modificar las tablas.
+	public static void modificar(String comando) {
+		if(conexion == null) {
+			try {
+				getConnection();
+				ps = conexion.prepareStatement(comando);
+				int res = ps.executeUpdate();
+				if(res>0) {
+					System.out.println("Persona Modificada");
+				}else System.out.println("Error al modificar persona");
+				conexion.close();
+			}catch(Exception e) {
+				System.err.print(e);
+			}
+		}
+	}
+
+	public static void eliminar(String comando) {
+		if(conexion == null) {
+			try {
+				getConnection();
+				ps = conexion.prepareStatement(comando);
+				int res = ps.executeUpdate();
+				if(res>0) {
+					System.out.println("Persona eliminada");
+				}else System.out.println("Error al eliminar persona");
+				conexion.close();
+			}catch(Exception e) {
+				System.err.print(e);
+			}
+		}
+	}
 }
