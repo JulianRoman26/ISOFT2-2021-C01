eccca1e25b9422512288f50d30a2a10d35423354
AaronPecesG <aaron.peces@alu.uclm.es>
2020-12-23 19:23:31 +0100

    testing junit

--- src/test/java/Dominio/Camarero_Barra_Test.java
+++ src/test/java/Dominio/Camarero_Barra_Test.java
@@ -8,7 +8,7 @@ public class Camarero_Barra_Test {
 
 	@Test
 	public void testAutenticar() {
-		Camarero_Barra cb = new Camarero_Barra(5);
+		Camarero_Barra cb = new Camarero_Barra(1);
 		boolean expected = true;
 		boolean actual = cb.Autenticar();
 		assertEquals(expected,actual);
--- src/test/java/Dominio/Camarero_MesaTest.java
+++ src/test/java/Dominio/Camarero_MesaTest.java
@@ -1,6 +1,7 @@
 package Dominio;
 
 import static org.junit.Assert.*;
+import java.util.*;
 
 import org.junit.Test;
 
@@ -9,7 +10,33 @@ public class Camarero_MesaTest {
 	@Test
 	public void test_ValidarComanda() {
 		Camarero_Mesa cm = new Camarero_Mesa(2,"Rafa","617002896");
-		cm.set
+		cm.setId_mesa(1);
+		Comanda comanda = new Comanda(1,1);
+		ArrayList<Plato> e = new ArrayList<Plato>();
+		ArrayList<Plato> p = new ArrayList<Plato>();
+		ArrayList<Plato> s = new ArrayList<Plato>();
+		ArrayList<Plato> t = new ArrayList<Plato>();
+		ArrayList<Bebida> b = new ArrayList<Bebida>();
+		
+		Plato entrante = new Plato("queso");
+		Plato primero= new Plato("ensalada");
+		Plato segundo= new Plato("lubina");
+		Plato postre= new Plato("tarta_de_queso");
+		Bebida bebida= new Bebida("agua");
+		e.add(entrante);
+		p.add(primero);
+		s.add(segundo);
+		t.add(postre);
+		b.add(bebida);
+		comanda.setBebidas(b);
+		comanda.setEntrantes(e);
+		comanda.setPrimer_plato(p);
+		comanda.setSegundo_plato(s);
+		comanda.setPostre(p);
+		
+		boolean espected = true;
+		boolean actual = cm.validarComanda(comanda);
+		assertEquals(espected,actual); 
 	}
 
 }
