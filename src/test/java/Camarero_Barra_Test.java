04a64ef6160b1a27e2213f8f8a700fc5c4156806
Julian Roman <Julian.Roman@alu.uclm.es>
2020-12-23 19:54:03 +0100

    JUnit v.1.1.0

--- src/test/java/Camarero_Barra_Test.java
+++ src/test/java/Camarero_Barra_Test.java
@@ -1,12 +1,19 @@
+
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import Dominio.Camarero_Barra;
+
 public class Camarero_Barra_Test {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAutenticar() {
+		Camarero_Barra cb = new Camarero_Barra(1);
+		boolean expected = true;
+		boolean actual = cb.Autenticar();
+		assertEquals(expected,actual);
 	}
 
 }
--- src/test/java/Gestor_ComandasTest.java
+++ src/test/java/Gestor_ComandasTest.java
@@ -2,11 +2,15 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import Dominio.Gestor_Comandas;
+
 public class Gestor_ComandasTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testIdentificarse() {
+		Gestor_Comandas gc = new Gestor_Comandas();
+		boolean expected=true;
+		boolean actual=gc.identificarse("Cocinero", 3);
+		assertEquals(expected,actual);
 	}
-
 }
